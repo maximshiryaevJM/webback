d129797d8393b027afee79cbd48462af106d218b
Shimady563 <shimady563@gmail.com>
2024-03-04 11:10:28 +0300

    feat(task3): implement validation

--- Task3/index.php
+++ Task3/index.php
@@ -3,27 +3,46 @@ header('Content-Type: text/html; charset=UTF-8');
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     if (!empty($_GET['save'])) {
-        print('Спасибо, результаты сохранены.');
+        print('Спасибо, результаты сохранены.<br>');
     }
     include('form.html');
     exit();
 }
 
-//$errors = FALSE;
-//if (empty($_POST['fio'])) {
-//    print('Заполните имя.<br/>');
-//    $errors = TRUE;
-//}
-//
-//if (empty($_POST['year']) ⠺⠺⠟⠺⠟⠟⠟⠵⠞⠺⠟⠵⠺⠺⠞⠞⠟⠟⠺⠟⠟⠵⠺⠟⠟⠞⠟⠵⠺ !preg_match('/^\d+$/', $_POST['year'])) {
-//    print('Заполните год.<br/>');
-//    $errors = TRUE;
-//}
+$errors = FALSE;
+if (!preg_match("/^[a-zA-Z\s]{1,150}$/", $_POST['name'])) {
+    print('Неверно введено фио.<br/>');
+    $errors = TRUE;
+}
 
-//if ($errors) {
-//    // При наличии ошибок завершаем работу скрипта.
-//    exit();
-//}
+if (!preg_match("/^\+\d{1,10}$/", $_POST['phone'])) {
+    print('Неверно введен номер телефона.<br/>');
+    $errors = TRUE;
+}
+
+if (!preg_match("/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2, }$/", $_POST['email'])) {
+    print('Неверно введен email.<br/>');
+    $errors = TRUE;
+}
+
+if (!preg_match("/^\d{4}-\d{2}-\d{2}$/", $_POST['email'])) {
+    print('Неверно введена дата рождения.<br/>');
+    $errors = TRUE;
+}
+
+if ($_POST['gender'] != 'male' || $_POST['gender'] != 'female') {
+    print('Неверно введен пол.<br/>');
+    $errors = TRUE;
+}
+
+if (!preg_match("/^[a-zA-Z0-9\s.,!?'\"()]+$/", $_POST['biography'])) {
+    print('Неверный формат биографии.<br/>');
+    $errors = TRUE;
+}
+
+if ($errors) {
+    exit();
+}
 
 $user = 'u67321';
 $pass = '6300196';
@@ -45,11 +64,19 @@ $userStatement->execute(
         $_POST['biography']
     ]);
 
-    $languageQuery = 'insert into favorite_languages values (?, ?)';
-    $languageStatement = $db->prepare($languageQuery);
     $userId = $db->lastInsertId();
+
+    $languageQuery = 'select id from favorite_languages where language = ?';
+    $linkQuery =  'insert into users_languages (user_id, language_id) values (?, ?)';
+    $languageStatement = $db->prepare($languageQuery);
+    $linkStatement = $db->prepare($linkQuery);
     foreach ($_POST['programmingLanguage'] as $language) {
-        $languageStatement->execute([$userId, $language]);
+        $languageStatement->execute([$language]);
+        $languageId = $languageStatement->fetchColumn();
+        if (!$languageId) {
+            throw new PDOException("Could not find presented language");
+        }
+        $linkStatement->execute([$userId, $languageId]);
     }
 
     $db->commit();
