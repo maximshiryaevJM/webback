dbd4638db09ddb73df58b8d7d5df60bf21bfeb0b
maximshiryaevJM <maximchic12345@gmail.com>
2024-05-21 19:59:12 +0000

    feat(task7): implement task7

--- Task7/repository/connection.php
+++ Task7/repository/connection.php
@@ -1,6 +1,6 @@
 <?php
 include  __DIR__ . '/../../variables.php';
 
-$db = new PDO('mysql:host=localhost;dbname=u67321', $user, $pass,
+$db = new PDO('mysql:host=localhost;' . $dbname, $user, $pass,
     [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
